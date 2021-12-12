07f2fa58179eafcfbb2403060812da55615f9550
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-12-12 19:42:44 -0300

    Adicionado rotas com crud de usuário

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
 var express = require('express');
+const user = require("../database/user");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/list', async function (req, res, next) {
+  res.send(await user.getUser());
 });
+
+router.post("/edit/:id/:name", async function (req, res) {
+  let resposta = {status: true}
+  if (req.params.name === null || req.params.name === undefined || req.params.name === "") {
+    res.statusCode = 500;
+    resposta.status = false;
+    resposta.error = "O nome da tarefa não pode ser vazio!";
+    res.json(resposta);
+    return
+  }
+  await user.updateUser(req.params.id, {name: req.params.name});
+  res.json(resposta);
+})
+
+router.post("/:name", async function (req, res) {
+  let resposta = {status: true}
+  if (req.params.name === null || req.params.name === undefined || req.params.name === "") {
+    res.statusCode = 500;
+    resposta.status = false;
+    resposta.error = "O nome da tarefa não pode ser vazio!";
+    res.json(resposta);
+    return
+  }
+  await user.insertUser({name: req.params.name});
+  res.json(resposta);
+})
+
+router.delete("/:id", function (req, res){
+  let resposta = {status: true}
+  if (!user.delete(req.params.id)){
+    res.statusCode = 500;
+    resposta.status = false;
+    resposta.error = "Falha ao exlcuir o registro";
+  }
+  res.json(resposta);
+})
 
 module.exports = router;
